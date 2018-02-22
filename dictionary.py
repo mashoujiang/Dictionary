328136ea3240a884c6b65e57977de0bc0043ac2a
shoujiangma <shoujiangma@163.com>
2018-02-22 12:06:57 +0800

    implement get meaning from youdao.com

--- dictionary.py
+++ dictionary.py
@@ -1,9 +1,15 @@
+# -*- coding:utf-8 -*-
 # Author: Ma, Shoujiang
 # Data: 2/20/2018
 # Describe: Build a dictionary based on my own needs.
 
+import hashlib
+import json
 import os.path
 import re
+import time
+import urllib
+import urllib2
 
 import pandas as pd
 
@@ -12,12 +18,19 @@ class Dictionary(object):
     def __init__(self, dic_name):
         if not os.path.isfile(dic_name):
             open(dic_name, 'a').close()
+
         self.total_words = 0
         self.dic_name = dic_name
+        self.columns = ['英', '汉']
+
+        self.url = 'http://fanyi.youdao.com/translate?' \
+                   'smartresult=dict&smartresult=rule'
+
         try:
-            self.dic = pd.read_csv(dic_name, squeeze=True, header=None)
+            self.dic = pd.read_csv(dic_name)
         except pd.io.common.EmptyDataError:
-            self.dic = pd.Series(dtype=str)
+            self.dic = pd.DataFrame(columns=self.columns, dtype=str)
+            self.dic.to_csv(self.dic_name, encoding="utf-8", index=False)
 
     def read_text(self, file_name):
         with open(file_name, 'r') as file:
@@ -37,26 +50,61 @@ class Dictionary(object):
             for word in line:
                 if not self.word_exist(word):
                     words.append(word)
-
         words = self.delete_duplicate(words)
-        self.to_dic(words)
+        word_mean = self.zip_word_and_mean(words)
+        self.to_dic(word_mean)
+
+    def word_exist(self, word):
+        return True if self.dic['英'][self.dic['英'] == word].size else False
 
     def delete_duplicate(self, words):
         new_words = list(set(words))
         return new_words
 
-    def word_exist(self, word):
-        return True if self.dic[self.dic == word].size else False
+    def zip_word_and_mean(self, words):
+        meanings = []
+        for word in words:
+            meaning = self.translate(word)
+            meanings.append(meaning)
+        return zip(words, meanings)
 
     def to_dic(self, words):
-        df = pd.Series(words)
-        df.to_csv(self.dic_name, mode='a+', header=False, index=False)
+        df = pd.DataFrame(words, columns=self.columns)
+        df.to_csv(self.dic_name, mode='a', encoding="utf-8", header=False, index=False)
 
-    def get_words_frequency(self):
-        pass
+    def translate(self, input):
+        data = self.form_data(input)
+        data = urllib.urlencode(data)
+        request = urllib2.Request(self.url, data=data)
+        response = urllib2.urlopen(request)
+        html = response.read()
+        target = json.loads(html)
+        result = target["translateResult"][0][0]['tgt']
+        return result
 
-    def sort(self):
-        pass
+    def form_data(self, input):
+        salt = str(int(time.time() * 1000))
+        client = 'fanyideskweb'
+        a = "rY0D^0'nM0}g5Mm1z%1G4"
+        md5 = hashlib.md5()
+        digStr = client + input + salt + a
+        md5.update(digStr)
+        sign = md5.hexdigest()
+
+        data = {}
+        data['i'] = input
+        data['from'] = 'AUTO'
+        data['to'] = 'AUTO'
+        data['smartresult'] = 'dict'
+        data['client'] = 'fanyideskweb'
+        data['salt'] = salt
+        data['sign'] = sign
+        data['doctype'] = 'json'
+        data['version'] = '2.1'
+        data['keyfrom'] = 'fanyi.web'
+        data['action'] = 'FY_BY_CLICKBUTTION'
+        data['typoResult'] = 'false'
+        return data
 
 
 if __name__ == '__main__':
